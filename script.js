8c2a37763520207ad267b16a6500153f34d864c0
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-07-16 13:40:04 -0300

    conhecendo o Shallow freezing

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Shallow Copy (cópia superficial): não pega os itens aninhados.
 
-const htmlCourse = {
-  course: "HTML",
-  students: [
-    {
-      name: "Jonas",
-      email: "jonas@email.com",
-    },
-  ],
-};
+// const htmlCourse = {
+//   course: "HTML",
+//   students: [
+//     {
+//       name: "Jonas",
+//       email: "jonas@email.com",
+//     },
+//   ],
+// };
 
 // const jsCourse = {
 //   ...htmlCourse,
@@ -30,14 +30,36 @@ const jsCourse = {
 jsCourse.students.push({ nome: "joão", email: "joao@email.com" });
 */
 
-const jsCourse = {
-  ...htmlCourse,
-  course: "Javascript",
+// const jsCourse = {
+//   ...htmlCourse,
+//   course: "Javascript",
+// };
+
+// jsCourse.students = [
+//   ...htmlCourse.students,
+//   { name: "tantofaz", email: "tantofaz@email.com" },
+// ];
+
+// console.log(htmlCourse, jsCourse);
+
+const book = {
+  title: "Objetos Imutáveis",
+  category: "Javascript",
+  author: {
+    name: "Jonas",
+    email: "jonas@email.com",
+  },
 };
 
-jsCourse.students = [
-  ...htmlCourse.students,
-  { name: "tantofaz", email: "tantofaz@email.com" },
-];
+// O Javascript em si não impõe restrições à modificação dos objetos.
+book.category = "HTML";
 
-console.log(htmlCourse, jsCourse);
+// Congela o objeto e impede a modificação.
+Object.freeze(book); // O FREEZE APENAS RESTRINGE O SHALLOW(RASO), OBJETOS ANINHADOS(DEEP) ELE NÃO RESTRINGE.
+book.category = "CSS"; // NÃO MODIFICA.
+
+book.author.name = "João"; // AQUI O FREEZE JÁ NÃO RESTRINGE MAIS
+
+// RESUMO: O Object.freeze() não impede modificações profundas em objetos aninhados (shallow freezing)
+
+console.log(book);
